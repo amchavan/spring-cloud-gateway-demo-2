8610df05a1a859d113bb30c1efabc612a81aedd6
amchavan <amchavan>
2019-09-13 18:40:44 +0200

    First working version

--- book-service/src/main/java/alma/obops/springcloud/bookservice/Controller.java
+++ book-service/src/main/java/alma/obops/springcloud/bookservice/Controller.java
@@ -1,5 +1,7 @@
 package alma.obops.springcloud.bookservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class Controller {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( Controller.class.getSimpleName() );
 
     @GetMapping( "/book/{id}" )
     public alma.obops.springcloud.bookservice.Book findById(@PathVariable String id ) {
@@ -18,6 +22,7 @@ public class Controller {
 
         int year = ThreadLocalRandom.current().nextInt( 1900, 2019 );
         u.year = Integer.toString( year );
+        LOGGER.info( "id=" + id + " ==> " + u.toString() );
         return u;
     }
 }
--- gateway/src/main/java/alma/obops/springcloud/gateway/Application.java
+++ gateway/src/main/java/alma/obops/springcloud/gateway/Application.java
@@ -2,10 +2,38 @@ package alma.obops.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    @Bean
 
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get/**")
+                        .filters(f -> f.rewritePath("/get/(?<id>.*)", "/book-service/book/${id}"))
+                        .uri("http://localhost:10000"))
+                .route(p -> p
+                        .header( "Fallback", "true" )
+                        .filters(f -> f.hystrix(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder ) {
+
+//        return builder.routes()
+//                .route( p -> p
+//                        .path( "/book" )
+////                        .uri( "http://localhost:7000/book-service" ))
+//                        .uri( "http://httpbin.org:80" ))
+//                .build();
+//    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
