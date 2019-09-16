7bac6e7757b66641bfd13e38847caa833395a3b4
amchavan <amchavan>
2019-09-16 09:59:26 +0200

    Minor changes

--- gateway/src/main/java/alma/obops/springcloud/gateway/Application.java
+++ gateway/src/main/java/alma/obops/springcloud/gateway/Application.java
@@ -13,27 +13,19 @@ public class Application {
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
-                        .path("/get/**")
-                        .filters(f -> f.rewritePath("/get/(?<id>.*)", "/book-service/book/${id}"))
-                        .uri("http://localhost:10000"))
-                .route(p -> p
-                        .header( "Fallback", "true" )
-                        .filters(f -> f.hystrix(config -> config
-                                .setName("mycmd")
-                                .setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80"))
+                        .path("/book/**")
+                        .filters(f -> f.rewritePath("/book/(?<id>.*)", "/book-service/book/${id}"))
+                        .uri("http://localhost:10000")
+                        .id( "book1" ))
+//                .route(p -> p
+//                        .header( "Fallback", "true" )
+//                        .filters(f -> f.hystrix(config -> config
+//                                .setName("mycmd")
+//                                .setFallbackUri("forward:/fallback")))
+//                        .uri("http://httpbin.org:80"))
                 .build();
     }
 
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder ) {
-
-//        return builder.routes()
-//                .route( p -> p
-//                        .path( "/book" )
-////                        .uri( "http://localhost:7000/book-service" ))
-//                        .uri( "http://httpbin.org:80" ))
-//                .build();
-//    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
